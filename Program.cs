1c35382fb38790ae06f707cb71c8cdd5259819d8
Matthew Kelly <mkonabike@gmail.com>
2019-04-20 11:29:48 -0500

    basic addition with int

--- Program.cs
+++ Program.cs
@@ -6,8 +6,12 @@ namespace hello
     {
         static void Main(string[] args)
         {
+			int a = 1;
+			int b = 2;
+			int c = a + b;
             Console.WriteLine("Hello World!");
 			Console.WriteLine("The current day and time is " + DateTime.Now);
+			Console.WriteLine(c);
         }
     }
 }
